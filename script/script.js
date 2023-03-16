7b6ded5c49031e764fb71f417452fbcc2348d438
Thiago dos Santos <90911509+thiago-dosantos@users.noreply.github.com>
2023-03-16 09:47:43 -0300

    testes script

--- script/script.js
+++ script/script.js
@@ -63,30 +63,30 @@ $(document).ready(function(){
     };
     
     
-    // (function(){
-    //     var $target = $('.anime'),
-    //             animationClass = 'anime-start',
-    //             offset = $(window).height() * 3/4;
+    (function(){
+        var $target = $('.anime'),
+                animationClass = 'anime-start',
+                offset = $(window).height() * 3/4;
     
-    //     function animeScroll() {
-    //         var documentTop = $(document).scrollTop();
+        function animeScroll() {
+            var documentTop = $(document).scrollTop();
     
-    //         $target.each(function(){
-    //             var itemTop = $(this).offset().top;
-    //             if (documentTop > itemTop - offset) {
-    //                 $(this).addClass(animationClass);
-    //             } else {
-    //                 $(this).removeClass(animationClass);
-    //             }
-    //         });
-    //     }
+            $target.each(function(){
+                var itemTop = $(this).offset().top;
+                if (documentTop > itemTop - offset) {
+                    $(this).addClass(animationClass);
+                } else {
+                    $(this).removeClass(animationClass);
+                }
+            });
+        }
     
-    //     animeScroll();
+        animeScroll();
     
-    //     $(document).scroll(debounce(function(){
-    //         animeScroll();
-    //     }, 200));
-    // })();
-    // console.log('teste')
+        $(document).scroll(debounce(function(){
+            animeScroll();
+        }, 200));
+    })();
+    console.log('teste')
 });
 
