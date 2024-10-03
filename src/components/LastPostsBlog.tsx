a2a2710d5a27ea4b7ddf2e876689d4ccc5163e05
Thiago dos Santos <thiago-dosantos@outlook.com>
2024-10-03 13:33:01 -0300

    if necessary
    Parameter for searchParams if necessary

--- src/components/LastPostsBlog.tsx
+++ src/components/LastPostsBlog.tsx
@@ -1,18 +1,22 @@
 import { wisp } from "@/lib/wisp";
 import { LastsPost } from "@/components/SectionsComponents/LastPosts";
 
-const LastPostsBlog  = async ({
+const LastPostsBlog = async ({
   searchParams,
 }: {
   searchParams: { [key: string]: string | string[] | undefined };
 }) => {
   const result = await wisp.getPosts({ limit: 6 });
+  const filteredPosts = searchParams.tag
+    ? result.posts.filter((post) => post.tags.some(tag => tag.name === searchParams.tag))
+    : result.posts;
+
   return (
     <section id='portfolio' className='section'>
-        <h1 className="text-center cursor-default text-3xl md:text-4xl font-bold tracking-tighter leading-tight py-12">
-          Recents Posts
-        </h1>
-      <LastsPost posts={result.posts} />
+      <h1 className="text-center cursor-default text-3xl md:text-4xl font-bold tracking-tighter leading-tight py-12">
+        Recent Posts
+      </h1>
+      <LastsPost posts={filteredPosts} />
     </section>
   );
 };
