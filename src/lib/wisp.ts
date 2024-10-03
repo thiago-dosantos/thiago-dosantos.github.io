5331d2e632e07f2ffe51cc4dd745b4fee0bbf933
Thiago dos Santos <thiago-dosantos@outlook.com>
2024-10-03 14:01:01 -0300

    Update wisp.ts

--- src/lib/wisp.ts
+++ src/lib/wisp.ts
@@ -6,7 +6,7 @@ import {
 } from "@wisp-cms/client";
 
 export const wisp = buildWispClient({
-  blogId: config.wisp.blogId,
+  blogId: config.wisp.blogId || "cm1p1vbnu0000xlxs3lwe4cxi",
 });
 
 export type { GetPostsResult, GetPostResult };
