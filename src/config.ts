a7aa26ccf2a67062871b1b6fef25d2a9af5813bf
Thiago dos Santos <thiago-dosantos@outlook.com>
2024-10-03 13:55:23 -0300

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,5 @@
 const buildConfig = () => {
   const blogId = process.env.NEXT_PUBLIC_BLOG_ID;
-  if (!blogId) throw new Error("NEXT_PUBLIC_BLOG_ID is missing");
   const name = process.env.NEXT_PUBLIC_BLOG_DISPLAY_NAME || "Thiago.";
   const copyright = process.env.NEXT_PUBLIC_BLOG_COPYRIGHT || "Thiago";
   const defaultTitle =
