2f791e6a2a749d703cfeb2b06fd4490346969b94
Thiago dos Santos <thiago-dosantos@outlook.com>
2024-10-03 13:34:38 -0300

    Remove cn
    remove cn, not necessary

--- src/components/SectionsComponents/Projects.tsx
+++ src/components/SectionsComponents/Projects.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState, useEffect } from 'react';
-import { cn } from "@/lib/utils";
 import { projectsData } from '@/data';
 import { projectsNav } from '@/data';
 import { StaticImageData } from 'next/image';
