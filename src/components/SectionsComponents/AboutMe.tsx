c726a26aad4137985f72b5d832e3a3ce7085c123
Thiago dos Santos <thiago-dosantos@outlook.com>
2024-10-04 10:58:04 -0300

    Update About Page
    Added socials links and curriculum download using buttons

--- src/components/SectionsComponents/AboutMe.tsx
+++ src/components/SectionsComponents/AboutMe.tsx
@@ -4,16 +4,17 @@ import Image from 'next/image';
 import TypingEffect from "./TypingEffect";
 import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { social } from "@/data";
 
 export const AboutMe: FunctionComponent = () => {
  return (
-    <section id='' className="lg:min-h-[50vh] min-h-[70vh] flex items-center lg:bg-cover lg:bg-center lg:bg-no-repeat py-16 lg:py-32 overflow-hidden">
+    <section className="lg:min-h-[50vh] min-h-[70vh] flex items-center lg:bg-cover lg:bg-center lg:bg-no-repeat py-16 lg:py-32 overflow-hidden">
         <div className='flex flex-col xl:flex-row gap-24'>
                 <Image 
                     src="/assets/about.webp" 
                     alt="Description of image" 
                     width={500} 
-                    height={300}
+                    height={500}
                     className="rounded-lg"
                 />
             <div className='flex flex-col items-center text-center lg:items-start lg:text-left'>
@@ -28,17 +29,27 @@ export const AboutMe: FunctionComponent = () => {
                         I&apos;m a proactive person who is always in search of knowledge and new experiences.
                     </p>
                 </div>
-                <div className= "flex space-x-4">
-                <Button asChild className="mt-4">
-                    <Link href="/">
-                        Download CV (PT-BR)
-                    </Link>
-                </Button>
-                <Button asChild className="mt-4">
-                    <Link href="/">
-                    Download CV (EN-UK)
-                    </Link>
-                </Button>
+                <div className= "flex flex-col items-center justify-center w-full space-x-4 max-h-[10vh]">
+                    <div className= "flex space-x-4 justify-center">
+                        <Button asChild className="mt-4">
+                            <Link href="/files/CVptbr.pdf" download target="_blank" rel="noopener noreferrer">
+                                Download CV (PT-BR)
+                            </Link>
+                        </Button>
+                        <Button asChild className="mt-4">
+                            <Link href="files/CVenuk.pdf" download target="_blank" rel="noopener noreferrer">
+                            Download CV (EN-UK)
+                            </Link>
+                        </Button>
+                    </div>
+                    <div className="text-xs text-muted-foreground hidden lg:flex flex space-x-6 p-8 justify-center">
+                        {social.map((item, index) => {
+                            const {href, icon} = item;
+                            return (
+                                <a className='hover:text-[#F63D3C] text-base' href={href} key={index}>{icon}</a>
+                            )
+                        })}
+                    </div>
                 </div>
             </div>
         </div>
